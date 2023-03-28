319cb7227e64d60f1d4e0de73275c369fab32334
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-28 14:52:23 +0300

    Add initial files

---
+++ schema.sql
@@ -0,0 +1,11 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+    id                  INT GENERATED ALWAYS AS IDENTITY,
+    name                VARCHAR(250),
+    date_of_birth       DATE,
+    escape_attempts     SMALLINT,
+    neutered            BOOLEAN,
+    weight_kg           DECIMAL,
+    PRIMARY KEY(id)
+);
