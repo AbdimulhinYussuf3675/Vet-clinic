97573cf674e4c6d7ba286eae702f76905da3ae5a
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-28 16:24:52 +0300

    Add sample data

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-/* Populate database with sample data. */
+/* Populate database with sample data */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
     VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
