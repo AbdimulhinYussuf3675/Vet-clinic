fd0e8e63a47937c2954e01632b59993c8743ddab
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-28 16:24:21 +0300

    Add database schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,17 @@
-/* Database schema to keep the structure of entire database. */
+/*Queries that provide answers to the questions from all projects.*/
 
-CREATE TABLE animals (
-    id                  INT GENERATED ALWAYS AS IDENTITY,
-    name                VARCHAR(250),
-    date_of_birth       DATE,
-    escape_attempts     SMALLINT,
-    neutered            BOOLEAN,
-    weight_kg           DECIMAL,
-    PRIMARY KEY(id)
-);
+SELECT * FROM animals WHERE name like '%mon';
+
+SELECT name FROM animals WHERE DATE_PART('Year', date_of_birth) BETWEEN 2016 AND 2019;
+
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered = true;
+
+SELECT * FROM animals WHERE name != 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
