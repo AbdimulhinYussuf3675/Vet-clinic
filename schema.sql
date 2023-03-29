986c5eda90e408adae77d7d9b5ec6638bb791f35
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-29 16:05:06 +0300

    Add species column to animals table

--- schema.sql
+++ schema.sql
@@ -7,3 +7,5 @@ CREATE TABLE IF NOT EXISTS animals (
     escape_attempts NUMERIC(5,2),
     neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL NOT NULL);
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(200);
